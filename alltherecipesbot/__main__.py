077cd464d1422705505154fd5c4376972c6f9971
wwooll <90984074+wwooll@users.noreply.github.com>
2023-08-09 20:24:26 -0700

    added a dev mode for testing

--- alltherecipesbot/__main__.py
+++ alltherecipesbot/__main__.py
@@ -1,14 +1,23 @@
 import allrecipes
 import bsky
+import os
 
 def main():
 	try:
 		(title, rating, recipe, photos) = allrecipes.get_random_recipe_and_photos()
 
 		text = title + "\n" + rating + "⭐"
-		bsky.post_to_bsky(text, recipe, photos)
+		if (os.getenv("PY_ENV", "prod") == "dev"):
+			print(title)
+			print(rating)
+			print(recipe)
+			print(photos)
+		else:
+			bsky.post_to_bsky(text, recipe, photos)
+
 	except RuntimeError:
-		bsky.frow_up()
+		if (os.getenv("PY_ENV", "prod") == "prod"):
+			bsky.frow_up()
 
 if __name__ == "__main__":
 	main()
